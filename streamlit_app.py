23ad38fbcac0a5b66a573cfe11f55f00f2d8ac6a
Karan Agarwal <karan@Karans-Laptop.local>
2025-08-19 17:10:49 +0530

    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,12 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import datetime
 
-# -----------------------------
-# Firebase Initialization
-# -----------------------------
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["FIREBASE_SERVICE_ACCOUNT"]))
+    cred = credentials.Certificate(st.secrets["FIREBASE_SERVICE_ACCOUNT"])
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://karanexports-d7f67-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
-
 # -----------------------------
 # UI: Order Form
 # -----------------------------
